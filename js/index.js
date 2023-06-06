3a7332d2655f387865b15c50009ac57ecc19954c
Cristian Rebora <cristian.rebora.prog@gmail.com>
2023-06-05 23:03:19 -0300

    fin primera pre entrega con cambio de opcion

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-const motos = [
+ const motos = [
     { modelo: '110cc', marca: 'Yamaha,Crypton', anio: 2019, kilometraje: 5000, color: 'Azul', precio: 250000 },
-    { modelo: '190cc', marca: 'Honda,Cb', anio: 2021, kilometraje: 0 , color: 'Negro', precio: 450000 },
+     { modelo: '190cc', marca: 'Honda,Cb', anio: 2021, kilometraje: 0 , color: 'Negro', precio: 450000 },
     { modelo: '1000cc', marca: 'Yamaha,R1', anio: 2019, kilometraje: 15000, color: 'Blanco', precio: 10800000 },
   ];
   
@@ -10,51 +10,52 @@ const motos = [
     for (let index = 0; index < motos.length; index++) {
       const moto = motos[index];
       catalogo = catalogo + (index + 1) + '. ' + moto.modelo + ' - ' + moto.marca + ' - ' + moto.anio + ' - ' + moto.kilometraje + ' km - ' + moto.color + ' - $' + moto.precio + '\n';
-    }
+   }
   
     alert(catalogo);
-  }
+   }
   
-  function comprarMoto() {
-    const seleccion = parseInt(prompt('Ingrese el número de la moto que desea comprar:')) - 1;
+   function comprarMoto() {
+     const seleccion = parseInt(prompt('Ingrese el número de la moto que desea comprar:')) - 1;
   
     if (seleccion >= 0 && seleccion < motos.length) {
       const motoSeleccionado = motos[seleccion];
   
-      const nombre = prompt('Ingrese su nombre completo:');
-      const direccion = prompt('Ingrese su dirección de entrega:');
+       const nombre = prompt('Ingrese su nombre completo:');
+     const direccion = prompt('Ingrese su dirección de entrega:');
       const tarjetaCredito = prompt('Ingrese el número de su tarjeta de crédito:');
   
       const confirmacion = 'Compra realizada exitosamente.\n\n moto: ' + motoSeleccionado.modelo + ' - ' + motoSeleccionado.marca + '\nNombre: ' + nombre + '\nDirección: ' + direccion + '\nTarjeta de crédito: ' + tarjetaCredito;
   
       alert(confirmacion);
     } else {
-      alert('Selección inválida');
-    }
-  }
+       alert('Selección inválida');
+ }
   
-  
-  function simuladorCompraMoto() {
+}
+   function simuladorCompraMoto() {
     let opcion = '';
   
-    do {
+     do {
       opcion = prompt('Seleccione una opción:\n\n1. Ver catálogo de motos\n2. Comprar moto\n3. Salir');
   
       switch (opcion) {
-        case '1':
-          mostrarCatalogo();
-          break;
+     case '1':
+       mostrarCatalogo();
+        break;
         case '2':
-          comprarMoto();
-          break;
-        case '3':
-          alert('Gracias por utilizar nuestra página oficial');
-          break;
-        default:
-          alert('Opción inválida');
+         comprarMoto();
+         break;
+       case '3':
+      alert('Gracias por utilizar nuestra página oficial');
+        break;
+      default:
+         alert('Opción inválida');
           break;
       }
     } while (opcion !== '3');
-  }
+   }
   
   simuladorCompraMoto();
+
+
